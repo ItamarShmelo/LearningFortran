1a8c3aa0d80f9e95d3809367b773878b872df46d
ItamarShmelo <itamargiron@gmail.com>
2021-03-08 13:32:07 +0200

    changed the timer cpu_time doesnot work for openmp

--- SimpleOMP/calc_pi.f90
+++ SimpleOMP/calc_pi.f90
@@ -5,9 +5,9 @@ program calculate_pi
     integer            :: i
     real(8), parameter :: delta_x = 1.0 / N
     real(8)            :: sum, my_pi
-    real               :: time_start, time_end
+    real(8)            :: time_start, time_end
 
-    call cpu_time(time_start)
+    time_start = get_time()
     write(*, *) N, delta_x, f(0.5d0)    
     
     sum = 0.0d0
@@ -22,11 +22,20 @@ program calculate_pi
     my_pi = 4.0d0 * sum
     write(*, *) "Pi = ", my_pi
 
-    call cpu_time(time_end)
+    time_end = get_time()
     write(*, *) "Time taken ", time_end - time_start, " seconds"
     contains
     real(8) function f(x)
         real(8), intent(in) :: x
         f = sqrt(1.0d0 - x ** 2)
     end function f
+
+    real(8) function get_time()
+        integer :: count, max_count, count_rate
+        call system_clock(count, count_rate, max_count)
+        
+        ! write(*, *) count, count_rate, max_count
+        ! write(*, *) real(count, 8) / real(count_rate, 8)
+        get_time = (real(count, 8) / real(count_rate, 8))
+    end function get_time
 end program calculate_pi
