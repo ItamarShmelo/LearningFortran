20c5f73163ef1b79ca429381c5591a8e85ea429a
ItamarShmelo <itamargiron@gmail.com>
2021-03-08 11:12:17 +0200

    Exercise 15.2 in Eijkhout - Parallel Programming in MPI and OpenMP

--- SimpleOMP/omp_1.f90
+++ SimpleOMP/omp_1.f90
@@ -1,10 +1,18 @@
 program omp_1
     use omp_lib 
     implicit none
+
+    integer :: tsum
     
     write(*, *) "There are ", OMP_GET_NUM_PROCS(), "processors"
     
     !$omp parallel
     write(*, *) "There are ", OMP_GET_NUM_THREADS(), "threads"
     !$omp end parallel
+
+    tsum = 0
+    !$omp parallel
+    tsum = tsum + OMP_GET_THREAD_NUM()
+    !$omp end parallel
+    write(*, *) "Sum is ", tsum
 end program omp_1
