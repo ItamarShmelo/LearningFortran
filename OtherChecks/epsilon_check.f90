a7bf75a57c166ce4cce05545d98e6ec82f5f517b
ItamarShmelo <itamargiron@gmail.com>
2021-04-11 12:05:35 +0300

    dont have to initialize them

--- OtherChecks/epsilon_check.f90
+++ OtherChecks/epsilon_check.f90
@@ -4,9 +4,6 @@ program epsilon_check
     real(8) :: sasha
     real(4) :: itamar
 
-    itamar = 1.0
-    sasha = 1.d0
-
     write(*, *) "epsilon real(4)", epsilon(itamar)
     write(*, *) "epsilon real(8)", epsilon(sasha)
     
